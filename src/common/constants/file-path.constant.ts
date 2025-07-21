6cebb446f259d5398ec4ef9960749e6551cbd951
Tohir Ravshanov <rth@MacBook-Pro-Tohir.local>
2025-07-21 11:18:30 +0500

    fx

--- src/common/constants/file-path.constant.ts
+++ src/common/constants/file-path.constant.ts
@@ -1,4 +1,7 @@
 export const FilePath = {
   LOGO: 'uploads/logo',
-  CATEGORY_ICON: 'uploads/category_icon',
+  CATEGORY_ICON: 'uploads/category_icons',
+  ABOUT_IMAGE: 'uploads/about',
+  ENCYCLOPEDIA_IMAGE: 'uploads/encyclopedia_images',
+  FIRSTAID_IMAGE: 'uploads/firstaid_images',
 };
